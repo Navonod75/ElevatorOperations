c274f205119a3f429daa573038b07753f9ab0029
Navonod75 <108223126+Navonod75@users.noreply.github.com>
2026-09-22 18:46:08 +0200

    Delete paddle_ocr_model.py
    irrelevant file

--- paddle_ocr_model.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-import pytesseract
-import numpy as np
-
-filename = 'references/20251110_130516.jpg'
-img1 = np.array(Image.open(filename))
-text = pytesseract.image_to_string(img1)
-
-print(text)
